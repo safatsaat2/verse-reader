9aee72d93e762f460f3834516fa89b6972b1d32f
Sakibur Rahman Safat <sakibsafat47@gmail.com>
2024-02-19 23:39:14 +0600

    Add files via upload

---
+++ hadith-random/script.js
@@ -0,0 +1,116 @@
+let ayat1 = `“He created the heavens and earth in truth and formed you and perfected your forms; and to Him is the [final] destination” [Quran 64:3]
+Meaning: It teaches that the final destination of every Muslim is Allah. He has created each person in the perfect form, and every life revolves around Allah. The verse inspires people when they feel lost or gloomy.`;
+let ayat2 = `“My mercy encompasses all things” [Quran 7:156]
+
+Meaning: It says that the mercy of Allah is limitless. There are no boundaries around. It inspires people who are repenting for sins and facing troubles in life. One must not be fearful because Allah forgives everyone.`;
+let ayat3 = `“So verily, with the hardship, there is a relief. Verily, with the hardship, there is relief” [Quran 94:5-6]
+
+Meaning: It asks people to have faith in Allah and says that one who recites the verses of the Quran when facing great difficulties in life gets blessings from Allah. He gives everyone and also takes something from all. This verse gives hope, and it is among the important ayat in the Quran.`;
+let ayat4 = `“And Allah would not punish them while they seek forgiveness” [Quran 8:33]
+
+Meaning: This verse teaches Muslims to ask for His mercy and forgiveness. It says that compassion is the most important virtue of life. One must always hope for the best and always have faith in the Almighty.`;
+let ayat5 = `“Do what is beautiful. Allah loves those who do what is beautiful” [Quran 2:195]
+
+Meaning: One must be pure at heart. The verse asks everyone to do good in this world. Our actions must be centered around doing good deeds that bring joy to the heart and offer something valuable to others as well.`;
+let ayat6 = `“Allah does not burden a soul beyond that it can bear” [Quran 2:286]
+
+Meaning: The ayah says that Allah tests everyone, and He does not put hurdles in one’s way if the person cannot handle them. It teaches that He knows us better, and He is aware of our strengths. He will not put devotees through something that they cannot handle.`;
+let ayat7 = `“And whoever relies upon Allah – then He is sufficient for him. Indeed, Allah will accomplish His purpose. Allah has already set for everything a [decreed] extent” [Quran 65:3]
+
+Meaning: When in struggle, A Muslim must always remember Allah, for His remembrance brings hope. One is never alone in this world; the supreme power is with everyone. The ayah restores faith in the Almighty.`;
+let ayat8 = `“The truth is from your Lord, so do not be among the doubters” [Quran 3:60]
+
+Meaning: The verse teaches everyone to believe in the oneness of Allah because it is the only truth in this world. It helps the mind to stay away from confusion and distractions that can bring anyone down.
+
+Muslims believe that when a person recites ayats from the Quran, Allah offers them guidance. When praying, the person can listen to His voice. If a person is sad, they get encouragement and comfort from the Almighty. Ayats offer a solution to people who feel lost in their lives. The verses hold significant importance among the followers of Islam.`;
+
+let ayat9 = `“And Allah would not punish them while they seek forgiveness” [Quran 8:33]
+
+In this verse, Allah reminds us of the power of his mercy and the importance of asking for forgiveness. Mercy and compassion are some of the most awe-inspiring acts of Allah, and as Muslims, we should take hope in the fact that with constant and sincere prayers for forgiveness, we can all be forgiven inshallah. This verse reminds us that we should always hold onto the hope that we can always ask for forgiveness and mercy.`
+;
+let ayat10 =`“He created the heavens and earth in truth and formed you and perfected your forms; and to Him is the [final] destination” [Quran 64:3]
+
+By reminding us all that the final destination for everyone on earth is back to Allah, the Quran emphasizes that our entire being and meaning of life revolves around this very fact: Allah has perfected us in our own unique form, and we will ultimately be returned to our Creator. By remembering that our final goal and destination should be about the return to Allah, this verse of the Quran serves as a timely source of inspiration whenever you feel lost in life.`;
+let ayat11 =`“And whoever relies upon Allah – then He is sufficient for him. Indeed, Allah will accomplish His purpose. Allah has already set for everything a [decreed] extent” [Quran 65:3]
+
+This verse serves as a source of hope for those who feel alone in this world; by remembering that Allah is and will be always enough for us all, we can take hope in the fact that we will never in fact be alone in this world. Knowing that Allah is there for us all can serve as an important reminder that all we ever need is faith in Allah and knowledge of the Holy Quran.`;
+let ayat12 =`“My mercy encompasses all things” [Quran 7:156]
+
+Another verse that reminders us of the mercy of Allah, this can help those who are fearful of not being forgiven despite sincere prayer and repentance. As this verse from the Quran states, Allah’s mercy encompasses everything and is boundless, meaning that no matter what we are asking forgiveness from, we should always ask for it with sincerity and inshallah we will be answered.`;
+let ayat13 =`“The truth is from your Lord, so do not be among the doubters” [Quran 3:60]
+
+Living in a world with endless distractions and confusions can wear one down immensely. This verse tells us that the one and only truth will always be from Allah, and that believing truthfully in the Oneness of God will help us both in this life and the hereafter.`;
+let ayat14 =`“Do what is beautiful. Allah loves those who do what is beautiful” [Quran 2:195]
+
+Another beautiful verse from the Quran, this can help remind us to stay good and pure in our actions, and to strive towards being the best Muslims even when it seems no one is looking. Beauty, on a deeper level, is often equated with goodness of Allah, and by remembering that our actions should be centered around having a beautiful heart we can inshallah come closer to Allah.`;
+let ayat15 =`“So verily, with the hardship, there is relief. Verily, with the hardship, there is relief” [Quran 94:5-6]
+
+One of the most quoted verses from the Quran, this particular verse reminds all who reads the Holy Quran that with every hardship in life, there will always be relief from Allah as well. Remembering that Allah gives and takes, and also provides us all with what is best, this particular verse can help those improve their levels of hope when it seems like all is lost.`;
+let ayat16 =` “The people will depart separated [into categories] to be shown [the result of] their deeds. So whoever does an atom’s weight of good will see it” [Quran 99:6-7]
+
+This verse serves as a timely reminder that even the smallest, seemingly most insignificant act of goodness will not go unnoticed by Allah. Encouraging us to do good even when it seems as if we are completely alone or at our lowest, this verse can give us inspiration in the goodness of every act of charity or kindness in this world, no matter what the scale may be.`;
+let ayat17 =`“Say, ‘O My servants who have transgressed against themselves [by sinning], do not despair of the mercy of Allah . Indeed, Allah forgives all sins. Indeed, it is He who is the Forgiving, the Merciful'” [Quran 39:53]
+
+Another verse of the Holy Quran that reminds us of the greatness of Allah’s mercy, this is another source of inspiration for those who need the gentle reminder that Allah will always be there for us. Knowing that with sincere repentance, Allah can and inshallah will forgive us, this verse is another reminder of the greatness of Allah’s mercy and compassion.`;
+let ayat18 =`(This is) a blessed Book which We have revealed to you, (Oh Muhammad), so that they may contemplate its verses, and people of reason may be mindful.
+
+(Surah Sad, 38:29)
+
+One of the reminders that can be found is the meaning of love. Love is widely mentioned in the Quran in many ways, such as the Divine qualities of Allah s.w.t. in multiple verses: Al-Ra-uuf (The kind), Al-Wali (The Protecting Friend), Al-Lateef (The Gentle), Al-Ghafur (The Forgiving), Ar-Rahim (The Merciful), Al-Wadud (The Affectionate Loving), just to name a few.`;
+let ayat19 =`and My Mercy encompasses everything
+
+(Surah Al-A’raf, 7:156)
+
+Love is a gift from The Creator for His creation. It is put into the hearts of humankind and brings joy to the soul. Love is the reason life continues to blossom. It is, for the human experience; a feeling of deep affection that may inspire commitment and sacrifice. `;
+let ayat20 =`Still, there are some who take others as Allah’s equal—they love them as they should love Allah—but the believers love Allah even more.
+
+(Surah Al-Baqarah, 2:165)
+
+In other words, love for others does not necessarily have to contradict with Allah’s love, as long as we love others for the sake of Allah s.w.t and in accordance to His pleasure. However, there are other forms of love that are warned to us by Allah s.w.t. These are the bad and prohibited ones, such as love for adultery and false deities.`;
+let ayat21 =`And if you should count the favours of Allah, you would never be able to number them. Indeed, Allah is Forgiving and Merciful
+
+(Surah An-Nahl, 16:18)
+
+To worship Allah while having gratitude in our hearts is one of the most fulfilling experiences in life. When we look at our lives and the wonders of the universe, we will definitely see that there is a Divine presence amidst this amazingly intelligent design in creation. `;
+let ayat22 =`Spend the cause of Allah and do not contribute to your destruction with your own hands, but do good, for Allah loves those who do good (virtuous)
+
+(Surah Al-Baqarah 2:195)
+
+To attain a fellow human being’s love, we generally do the things that he or she loves and avoid that which he or she hates. Allah’s love is far greater than the love any human being can manifest; in fact, it is the greatest love that we can attain.`;
+let ayat23 =`There certainly has come to you a messenger from among yourselves. He is concerned by your suffering, anxious for your well-being, and gracious and merciful to the believers
+
+(Surah At-Tawbah, 9:128)`;
+let ayat24 =`O mankind, be mindful of your Lord, who created you from one soul and created from it its mate and dispersed from both of them many men and women. And fear Allah, through whom you ask one another, and the wombs. Indeed, Allah is ever, over you, an Observer
+
+(Surah An-Nisa 4:1)
+
+Every human being is created from a soul which was then dispersed into men and women. Therefore, to each is his or her own mate, where some might refer to it as a soulmate. `;
+let ayat25 =`And of His signs is that He created for you from yourselves mates that you may find tranquillity in them, and He placed between you affection and mercy. Indeed, that are signs for people who give thought
+
+(Surah Ar-Rum 30:21)
+
+After marriage, partners tend to show sound affection and mercy to one another. This includes forgiving the other and becoming patient in facing challenges ahead. It is as clearly mentioned in the Quran that men can find tranquillity in women who are their soulmates and vice versa.`;
+let ayat26 =`Your spouses are a garment (comfort, chastity, and protection) for you as you are for them
+
+(Surah Al-Baqarah 2:187)
+
+The literal meaning of ‘libas’ as stated in the above verse means clothing. Clothes cover our flaws and defects, protect us from external harm and it surely compliments the way we look. Likewise, spouses cover defects and protect one another from any harm. Spouses also beautify one another, leading to joy and comfort. `;
+let ayat27 =`Without a doubt, Allah knows what they conceal and what they reveal. He certainly does not love those who are too proud (Arrogant)
+
+(Surah An-Nahl, 16:23)
+
+After all, the Quran is the book of revelation from Allah s.w.t from which we refer to for reminders about many things, including love. Indeed it is guidance from The Most Merciful, Most Loving.`;
+
+let quran = [ayat1, ayat2, ayat3, ayat4, ayat5, ayat6, ayat7, ayat8, ayat9, ayat10, ayat11, ayat12, ayat13, ayat14, ayat15, ayat16, ayat17, ayat18, ayat19, ayat20, ayat21, ayat22, ayat23, ayat24, ayat25, ayat26, ayat27];
+
+function hadithItems(ayat) {
+    return quran[Math.floor(Math.random() * ayat.length)];
+}
+
+document.getElementById('btn').addEventListener('click', function () {
+    const p = document.createElement('p');
+    p.className = 'pb-10 pt-5 border px-5'
+    p.innerText = hadithItems(quran);
+    const div = document.getElementById('ayat-container');
+    div.appendChild(p);
+})
